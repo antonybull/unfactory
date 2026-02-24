37b87ca823980789773bfc6bd0832e5de190a377
Antony Bull <yourrealemail@gmail.com>
2026-02-24 17:50:37 +0000

    whatever the fuck

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white p-8">
-      <h1 className="text-9xl md:text-12xl font-black tracking-tighter uppercase text-white mb-16">
+      <h1 className="text-9xl md:text-12xl font-black tracking-tighter lowercase text-white mb-16">
         unfactory
       </h1>
-      <div className="text-5xl md:text-7xl font-bold tracking-widest text-center mb-32">
-        rescue<br />
-        restore<br />
-        reuse<br />
-        reloved
-      </div>
+      <p className="text-5xl md:text-7xl font-mono font-light tracking-widest text-center mb-32">
+        rescued, restored, reused, reloved
+      </p>
       <p className="text-xl font-mono text-gray-400 text-center mb-16">
         ...to be continued
       </p>
