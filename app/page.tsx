dc191ad7c95881ccab8aee2a769bb154190b025b
Antony Bull <yourrealemail@gmail.com>
2026-02-25 15:19:38 +0000

    smaller rescued line no commas

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
       <h1 className="text-7xl md:text-9xl lg:text-10xl font-black tracking-tighter lowercase text-white mb-16">
         unfactory
       </h1>
-      <div className="text-4xl md:text-5xl lg:text-6xl font-mono font-light tracking-widest text-center mb-32">
-        rescued, restored, reused, reloved
+      <div className="text-4xl md:text-6xl lg:text-7xl font-mono font-light tracking-widest text-center mb-32">
+        rescued restored reused reloved
       </div>
       <p className="text-xl font-mono text-gray-400 text-center mb-16">
         we take old things made from wood and make new things from wood.<br />
