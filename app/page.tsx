5fc23f401fd1ed253d9b465198c0073807562412
antonybull <antonybull1@gmail.com>
2026-07-06 22:04:48 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,4 @@
-export default function Home() {
+ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white p-8">
-      <h1 className="text-7xl md:text-9xl lg:text-10xl font-black tracking-tighter lowercase text-white mb-16">
-        unfactory
-      </h1>
-      
-      </div>
-      
-      </p>
-      <p className="text-xl font-mono text-white text-center">
-        mail@unfactory.co.uk
-      </p>
-    </main>
-  );
-}
+      <h1 className="text-7xl md:text-9xl lg:text-10xl font
