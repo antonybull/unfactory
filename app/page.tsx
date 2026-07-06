5b81af58f3ee7e1028c92a40a856af958546139d
antonybull <antonybull1@gmail.com>
2026-07-06 22:15:53 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
- export default function Home() {
+export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white p-8">
       <h1 className="text-7xl md:text-9xl lg:text-10xl font
